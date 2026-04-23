d7441036d51dd157b52f139ec870e8ed293b925c
branRoberts <brandonkrob12@gmail.com>
2026-04-23 14:25:55 -0400

    Second Commit
    Added skeleton code and the README.md

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
 public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
 }
